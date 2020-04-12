5b1e76dffeff2db36847f40f0fb31a0c7d31f670
Liberbell <liberbell@gmail.com>
2020-04-12 16:01:22 +0900

    fileope2-7

--- fileope2.py
+++ fileope2.py
@@ -1,6 +1,10 @@
-file = open("data_file/example.com", "w")
-file.write("Let's check the write operation.")
-file.seek(6)
+# file = open("data_file/example.com", "w")
+# file.write("Let's check the write operation.")
+# file.seek(6)
 
-file.write(" examine ")
-file.close()
+# file.write(" examine ")
+# file.close()
+
+file = open("data_file/example.com", "r")
+for lines in file :
+    print(lines)
