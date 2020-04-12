11b4c4b980a4d49f4cfc8d2c7bdb2664e5be41a0
Liberbell <liberbell@gmail.com>
2020-04-12 16:06:20 +0900

    fileope2-11

--- fileope2.py
+++ fileope2.py
@@ -5,12 +5,15 @@
 # file.write(" examine ")
 # file.close()
 
-file = open("data_file/example.com", "r")
-for lines in file :
-    print(lines)
-file.close()
+# file = open("data_file/example.com", "r")
+# for lines in file :
+#     print(lines)
+# file.close()
 
-with open("data_file/example.com", "w") as f:
-    f.write("First line\n")
-    f.write("Second line\n")
-    f.write("Third line\n")
+# with open("data_file/example.com", "w") as f:
+#     f.write("First line\n")
+#     f.write("Second line\n")
+#     f.write("Third line\n")
+
+f = open("data_file/example.com", "a")
+print(f.tell())
