a92d9853155b88beb79aa6271921084f499c6db6
Liberbell <liberbell@gmail.com>
2020-04-13 12:05:11 +0900

    fileope3-4

--- fileope3.py
+++ fileope3.py
@@ -4,3 +4,9 @@ f = open("data_file/example.com", "a")
 print(f.tell())
 
 print(os.stat("data_file/example.com").st_size)
+
+print(f.truncate(37))
+f.close()
+
+f = open("data_file/example.com", "r")
+print(f.read())
