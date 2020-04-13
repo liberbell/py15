64abed98385278d037d9ec2bc3398c892b3a3c62
Liberbell <liberbell@gmail.com>
2020-04-13 12:06:16 +0900

    fileope3-4

--- fileope3.py
+++ fileope3.py
@@ -1,12 +1,12 @@
 import os
 
-f = open("data_file/example.com", "a")
-print(f.tell())
+# f = open("data_file/example.com", "a")
+# print(f.tell())
 
-print(os.stat("data_file/example.com").st_size)
+# print(os.stat("data_file/example.com").st_size)
 
-print(f.truncate(37))
-f.close()
+# print(f.truncate(37))
+# f.close()
 
 f = open("data_file/example.com", "r")
 print(f.read())
