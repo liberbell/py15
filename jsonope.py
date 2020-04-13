7c01c963112c4003dd1a2624d7d53115f42d58a5
Liberbell <liberbell@gmail.com>
2020-04-14 08:34:39 +0900

    jsonope5

--- jsonope.py
+++ jsonope.py
@@ -10,3 +10,7 @@ print(car)
 car_dict = json.loads(car)
 print(car_dict)
 print(car_dict['variants'])
+
+with open("data_file/currency.json", "r") as json_file:
+    data = json.loads(json_file)
+    print(data)
