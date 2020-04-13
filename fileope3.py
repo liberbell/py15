9a37cf012df3e297552473fe46947005b6422634
Liberbell <liberbell@gmail.com>
2020-04-13 12:31:44 +0900

    fileope3-5

--- fileope3.py
+++ fileope3.py
@@ -8,5 +8,9 @@ import os
 # print(f.truncate(37))
 # f.close()
 
-f = open("data_file/example.com", "r")
-print(f.read())
+# f = open("data_file/example.com", "r")
+# print(f.read())
+
+f = open("data_file/example.com", "r+")
+f.writelines("we are doing an 'r+' operation")
+f.close()
