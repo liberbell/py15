65d868af3638a5300ff3fff655ec136e6ec2645f
Liberbell <liberbell@gmail.com>
2020-04-17 16:46:54 +0900

    csvope2-4

--- csvope2.py
+++ csvope2.py
@@ -1,7 +1,13 @@
 import csv
 
-file = open("data_file/record.csv", "r")
+# file = open("data_file/record.csv", "r")
+# with file:
+#     read = csv.DictReader(file)
+#     for row in read:
+#         print(dict(row))
+
+file = open("data_file/record_tab.csv", "r")
 with file:
-    read = csv.DictReader(file)
+    read = csv.reader(file, dialect="tab")
     for row in read:
-        print(dict(row))
+        print(row)
