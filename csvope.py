24341c6be751fe7e15219d57befe34397688b66e
Liberbell <liberbell@gmail.com>
2020-04-17 16:34:14 +0900

    csvope3

--- csvope.py
+++ csvope.py
@@ -11,3 +11,9 @@ with file:
     read = csv.reader(file, delimiter="|")
     for row in read:
         print(row)
+
+file = open("data_file/record_tab.csv", "r")
+with file:
+    read = csv.reader(file)
+    for row in read:
+        print(row)
