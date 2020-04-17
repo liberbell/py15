64abbac4d8e26b328a2b0ef0f3a69d7c47b1928b
Liberbell <liberbell@gmail.com>
2020-04-17 16:31:29 +0900

    csvope1

--- csvope.py
+++ csvope.py
@@ -5,3 +5,9 @@ with file:
     read = csv.reader(file)
     for row in read:
         print(row)
+
+file = open("data_file/record_pipe.csv", "r")
+with file:
+    read = csv.reader(file)
+    for row in read:
+        print(row)
